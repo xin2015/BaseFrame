fa56b1564844cbc2bed9cee2d913be7c509a955f
LiuHaixin <liuhaixin2016@outlook.com>
2018-06-15 11:47:00 +0800

    FTPHelper

--- BaseFrame.Console/Program.cs
+++ BaseFrame.Console/Program.cs
@@ -4,6 +4,7 @@ using BaseFrame.Core.Helpers;
 using BaseFrame.DAL;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
@@ -15,11 +16,14 @@ namespace BaseFrame.Console
     {
         static void Main(string[] args)
         {
-            using (FluentModel db = new FluentModel())
-            {
-                db.UpdateSchema();
-                db.InitData();
-            }
+            //using (FluentModel db = new FluentModel())
+            //{
+            //    db.UpdateSchema();
+            //    db.InitData();
+            //}
+
+            FTPHelper helper = new FTPHelper("ftp://202.104.69.206/Data/Upload", "admin", "suncereltd@2017");
+            helper.DownloadFile2("20180611.zip", "D:\\20180611.zip");
         }
     }
 }
--- BaseFrame.Core/Helpers/FTPHelper.cs
+++ BaseFrame.Core/Helpers/FTPHelper.cs
@@ -8,55 +8,44 @@ using System.Threading.Tasks;
 
 namespace BaseFrame.Core.Helpers
 {
+    /// <summary>
+    /// FTP工具类
+    /// </summary>
     public class FTPHelper
     {
-        private NetworkCredential networkCredential;
-        private string baseUriString;
+        private NetworkCredential _networkCredential;
+        private string _baseUriString;
 
         public FTPHelper(string baseUriString, string userName, string password)
         {
-            this.baseUriString = baseUriString;
-            networkCredential = new NetworkCredential(userName, password);
+            this._baseUriString = baseUriString;
+            _networkCredential = new NetworkCredential(userName, password);
+        }
+
+        private string GetPath(string path)
+        {
+            return string.Format("{0}/{1}", _baseUriString, path);
+        }
+
+        private FtpWebRequest GetRequest(string path, string method)
+        {
+            FtpWebRequest request = (FtpWebRequest)WebRequest.Create(GetPath(path));
+            request.Credentials = _networkCredential;
+            request.Method = method;
+            return request;
         }
 
         private FtpWebResponse GetResponse(string path, string method)
         {
-            string requestUriString = string.Format("{0}/{1}", baseUriString, path);
-            FtpWebRequest request = (FtpWebRequest)WebRequest.Create(requestUriString);
-            request.Credentials = networkCredential;
-            request.Method = method;
-            return (FtpWebResponse)request.GetResponse();
+            return (FtpWebResponse)GetRequest(path, method).GetResponse();
         }
 
-        public string DeleteFile(string path)
-        {
-            FtpWebResponse response = GetResponse(path, WebRequestMethods.Ftp.DeleteFile);
-            string result = response.StatusDescription;
-            response.Close();
-            return result;
-        }
-
-        public byte[] DownloadFile(string path)
-        {
-            return GetData(path, WebRequestMethods.Ftp.DownloadFile);
-        }
-
-        public string[] ListDirectory(string path)
-        {
-            string result = GetString(path, WebRequestMethods.Ftp.ListDirectory);
-            return result.Split(new string[] { "\r\n" }, StringSplitOptions.RemoveEmptyEntries);
-        }
-
-        public string GetString(string path, string method, Encoding encoding)
-        {
-            FtpWebResponse response = GetResponse(path, method);
-            StreamReader sr = new StreamReader(response.GetResponseStream(), encoding);
-            string result = sr.ReadToEnd();
-            sr.Close();
-            response.Close();
-            return result;
-        }
-
+        /// <summary>
+        /// 获取字符串信息
+        /// </summary>
+        /// <param name="path">ftp相对路径</param>
+        /// <param name="method">方法</param>
+        /// <returns></returns>
         public string GetString(string path, string method)
         {
             FtpWebResponse response = GetResponse(path, method);
@@ -67,6 +56,12 @@ namespace BaseFrame.Core.Helpers
             return result;
         }
 
+        /// <summary>
+        /// 获取字节数组
+        /// </summary>
+        /// <param name="path">ftp相对路径</param>
+        /// <param name="method">方法</param>
+        /// <returns></returns>
         public byte[] GetData(string path, string method)
         {
             FtpWebResponse response = GetResponse(path, method);
@@ -76,6 +71,102 @@ namespace BaseFrame.Core.Helpers
             return result;
         }
 
+        /// <summary>
+        /// 删除文件
+        /// </summary>
+        /// <param name="path">ftp相对路径</param>
+        /// <returns></returns>
+        public string DeleteFile(string path)
+        {
+            FtpWebResponse response = GetResponse(path, WebRequestMethods.Ftp.DeleteFile);
+            string result = response.StatusDescription;
+            response.Close();
+            return result;
+        }
 
+        /// <summary>
+        /// 获取路径文件列表
+        /// </summary>
+        /// <param name="path">ftp相对路径</param>
+        /// <returns></returns>
+        public string[] ListDirectory(string path)
+        {
+            string result = GetString(path, WebRequestMethods.Ftp.ListDirectory);
+            return result.Split(new string[] { "\r\n" }, StringSplitOptions.RemoveEmptyEntries);
+        }
+
+        /// <summary>
+        /// 下载文件
+        /// </summary>
+        /// <param name="path">ftp相对路径</param>
+        /// <param name="fileName">文件本地保存路径</param>
+        public void DownloadFile(string path, string fileName)
+        {
+            using (WebClient wc = new WebClient())
+            {
+                wc.Credentials = _networkCredential;
+                wc.DownloadFile(GetPath(path), fileName);
+            }
+        }
+
+        /// <summary>
+        /// 下载文件（断点续传）
+        /// </summary>
+        /// <param name="path">ftp相对路径</param>
+        /// <param name="fileName">文件本地保存路径</param>
+        public void DownloadFileBreakpointContinuingly(string path, string fileName)
+        {
+            FileStream fs = new FileStream(fileName, FileMode.Append);
+            FtpWebRequest request = GetRequest(path, WebRequestMethods.Ftp.DownloadFile);
+            request.ContentOffset = fs.Length;
+            FtpWebResponse response = (FtpWebResponse)request.GetResponse();
+            response.GetResponseStream().CopyTo(fs);
+            response.Close();
+            fs.Close();
+        }
+
+        /// <summary>
+        /// 下载字节数组
+        /// </summary>
+        /// <param name="path">ftp相对路径</param>
+        /// <returns></returns>
+        public byte[] DownloadData(string path)
+        {
+            using (WebClient wc = new WebClient())
+            {
+                wc.Credentials = _networkCredential;
+                return wc.DownloadData(GetPath(path));
+            }
+        }
+
+        /// <summary>
+        /// 上传文件
+        /// </summary>
+        /// <param name="path">ftp相对路径</param>
+        /// <param name="fileName">文件本地保存路径</param>
+        /// <returns></returns>
+        public byte[] UploadFile(string path, string fileName)
+        {
+            using (WebClient wc = new WebClient())
+            {
+                wc.Credentials = _networkCredential;
+                return wc.UploadFile(GetPath(path), fileName);
+            }
+        }
+
+        /// <summary>
+        /// 上传字节数组
+        /// </summary>
+        /// <param name="path">ftp相对路径</param>
+        /// <param name="data">字节数组</param>
+        /// <returns></returns>
+        public byte[] UploadData(string path, byte[] data)
+        {
+            using (WebClient wc = new WebClient())
+            {
+                wc.Credentials = _networkCredential;
+                return wc.UploadData(GetPath(path), data);
+            }
+        }
     }
 }
